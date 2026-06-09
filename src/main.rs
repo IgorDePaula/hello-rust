b0c5026ae7e81450718ca731f2c4e70d6e972bea
Igor C. de Paula <principe.borodin@gmail.com>
2026-06-09 15:55:59 -0300

    async await

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,6 @@
-use std::collections::VecDeque;
-
-struct Fila<T> {
-    itens: VecDeque<T>
-}
-
-impl<T> Fila<T> {
-    fn new() -> Self {
-        Fila { itens: VecDeque::new() }
-    }
-
-    fn enfileirar(&mut self, item: T) {
-       self.itens.push_back(item);
-    }
-
-    fn desenfileirar(&mut self) -> Option<T> {
-        self.itens.pop_front()
-    }
-
-    fn tamanho(&self) -> usize {
-        self.itens.len()
-    }
-
-    fn esta_vazia(&self) -> bool {
-        self.itens.is_empty()
-    }
-}
-
-fn main() {
-    // teste com i32
-    let mut fila_nums: Fila<i32> = Fila::new();
-    fila_nums.enfileirar(10);
-    fila_nums.enfileirar(20);
-    fila_nums.enfileirar(30);
-    println!("Tamanho: {}", fila_nums.tamanho());
-    println!("Removeu: {:?}", fila_nums.desenfileirar());
-    println!("Removeu: {:?}", fila_nums.desenfileirar());
-    println!("Vazia: {}", fila_nums.esta_vazia());
-
-    // teste com String
-    let mut fila_nomes: Fila<String> = Fila::new();
-    fila_nomes.enfileirar("Ana".to_string());
-    fila_nomes.enfileirar("Borodin".to_string());
-    println!("Primeiro: {:?}", fila_nomes.desenfileirar());
+#[tokio::main]
+async fn main() {
+    println!("Iniciando...");
+    let resultado = soma_async(2, 3).await;  // .await executa o Future
+    println!("Resultado: {}", resultado);
 }
